1bb8867341255ac6512cea6f353aa4bc676d39cf
Filipe Garrote <filipe.garrote@gmail.com>
2022-06-15 01:07:03 -0300

    v3 - add Lógica números válidos: Embaralhar e distribuir cartas

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+//INÍCIO BASE DE CÁLCULOS
+
+let baseArray = [
+  document.querySelector(".card1"), 
+  document.querySelector(".card1"), 
+  document.querySelector(".card2"), 
+  document.querySelector(".card2"), 
+  document.querySelector(".card3"), 
+  document.querySelector(".card3"), 
+  document.querySelector(".card4"), 
+  document.querySelector(".card4"), 
+  document.querySelector(".card5"), 
+  document.querySelector(".card5"), 
+  document.querySelector(".card6"), 
+  document.querySelector(".card6"), 
+  document.querySelector(".card7"), 
+  document.querySelector(".card7")
+]
+
+function comparador() {
+  return Math.random() -0.5;
+}
+
+//INÍCIO BASE DE CÁLCULOS
+
 
 initial()
 
@@ -8,13 +33,29 @@ return Number(greets)
 
 function initial() {
   let greets = greetings()
+  let cardsArray = [];
+  let contador1 = 0
+  let contador2 = 0
+  let insert = document.querySelector(".container")
   if (greets%2 === 0 && greets >= 4 && greets <= 14){
-    
-    //EMBARALHAR A QTD DE CARTAS SOLICITADAS E DISTRIBUÍ-LAS NA MESA
-
+    while (contador1 < greets){
+      cardsArray.push(baseArray[contador1])
+      contador1++
+    }
+    cardsArray.sort(comparador)
+    while (contador2 < cardsArray.length){
+      insert.innerHTML = insert.innerHTML + `<div class="card">
+      ${cardsArray[contador2].innerHTML}
+      </div>`
+      contador2++
+      }
   } else {
     initial()
   }
-
 }
 
+function flip(){
+
+  //PENSAR COMO FAZER O FLIP INICIAL + FLIP PARA DESVIRAR. 
+
+}
