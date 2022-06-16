747fee3793555aad5a02755019b77a8cf7dc45ab
Filipe Garrote <filipe.garrote@gmail.com>
2022-06-15 23:04:00 -0300

    v4 - fix inserção dinâmica de cards + add flip cards

--- script.js
+++ script.js
@@ -1,61 +1,49 @@
 //INÍCIO BASE DE CÁLCULOS
 
-let baseArray = [
-  document.querySelector(".card1"), 
-  document.querySelector(".card1"), 
-  document.querySelector(".card2"), 
-  document.querySelector(".card2"), 
-  document.querySelector(".card3"), 
-  document.querySelector(".card3"), 
-  document.querySelector(".card4"), 
-  document.querySelector(".card4"), 
-  document.querySelector(".card5"), 
-  document.querySelector(".card5"), 
-  document.querySelector(".card6"), 
-  document.querySelector(".card6"), 
-  document.querySelector(".card7"), 
-  document.querySelector(".card7")
-]
+let baseArray = ["bobrossparrot.gif", "explodyparrot.gif","fiestaparrot.gif", "metalparrot.gif", 
+"revertitparrot.gif", "tripletsparrot", "unicornparrot.gif"]
 
 function comparador() {
-  return Math.random() -0.5;
+  return Math.random() - 0.5;
 }
 
 //INÍCIO BASE DE CÁLCULOS
 
-
-initial()
-
-function greetings() {
-let greets = prompt("Bem-vindo(a), my precious. Para começar o jogo, escolha a quantidade de cartas que deseja. (de 4 a 14)\nLembre-se: Quanto mais cartas, mais difícil. hehe")
-return Number(greets)
-}
+initial();
 
 function initial() {
-  let greets = greetings()
+  const greets = Number(prompt(
+    "Bem-vindo(a), my precious. Para começar o jogo, escolha a quantidade de cartas que deseja. (de 4 a 14)\nLembre-se: Quanto mais cartas, mais difícil. hehe"
+  ));
+  let container = document.querySelector(".container")
   let cardsArray = [];
-  let contador1 = 0
-  let contador2 = 0
-  let insert = document.querySelector(".container")
+
   if (greets%2 === 0 && greets >= 4 && greets <= 14){
-    while (contador1 < greets){
-      cardsArray.push(baseArray[contador1])
-      contador1++
-    }
-    cardsArray.sort(comparador)
-    while (contador2 < cardsArray.length){
-      insert.innerHTML = insert.innerHTML + `<div class="card">
-      ${cardsArray[contador2].innerHTML}
-      </div>`
-      contador2++
-      }
+    for (let i = 0; i < greets/2; i++){
+      cardsArray.push(baseArray[i], baseArray[i]);
+    } 
   } else {
     initial()
   }
+  cardsArray.sort(comparador);
+  for(let i = 0; i < cardsArray.length; i++){
+
+    container.innerHTML +=
+      `<div class="card" data-identifier="card" onclick="turnIt(this)">
+        <div class="back face" data-identifier="back-face">
+          <img src="./images/front.png" />
+        </div>
+        <div class="front face" data-identifier="front-face">
+          <img src="./images/${cardsArray[i]}" />
+        </div>
+      </div>`
+  }
 }
 
-function flip(){
-
-  //PENSAR COMO FAZER O FLIP INICIAL + FLIP PARA DESVIRAR. 
-
+function turnIt(element){
+  const front = element.querySelector(".front");
+  const back = element.querySelector(".back");
+  front.classList.add("turnFront")
+  back.classList.add("turnBack")
+  console.log("clicou")
 }
